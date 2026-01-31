e87b2a22354bed4c4a589769ea6cee77c178f9d2
SUSMITA-apt <sushmitahowlader96@gmail.com>
2026-01-31 23:46:00 +0900

    Initial commit

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-const {Buffer} = require('node:buffer');
-const buf = Buffer.from("Hello")
-buf [0] = 72
-// console.log(buf);
-console.log(buf.toString());
+// create a Buffer from a string
+// const {Buffer} = require('node:buffer');
+// const buf = Buffer.from("Hello")
+// buf [0] = 72
+// // console.log(buf);
+// console.log(buf.toString());
+
+// convert a Buffer to a JSON object
+const buf = Buffer.from("Hello");
+
+const json = buf.toJSON();
+console.log(json);
